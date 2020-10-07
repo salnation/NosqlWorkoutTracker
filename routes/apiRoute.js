0208f08f53f4db5b6e5413251bc8a3216b3b4c4d
Stefan Salituro <stefansalituro@gmail.com>
2020-10-06 20:20:55 -0400

    updated the readme and fixed some bugs causing my app to not deploy correctly.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,58 +1,36 @@
-var express = require("express");
-var router = express.Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/workout.js")
 
+module.exports = function (app) {
 
-module.exports = function(app) {
-  // middleware that is specific to this router
-  router.use(function timeLog(req, res, next) {
-    console.log("Time: ", Date.now());
-    next();
-  });
-  
-  router.get("/api/workouts", (req, res) => {
-    Workout.find()
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  router.post("/api/workouts", (req, res) => {
-  Workout.create({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
+    app.get("/api/workouts", function (req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
     });
-});
 
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Workout.findByIdAndUpdate(
-    params.id,
-    { $push: { exercises: body } },
-
-    { new: true, runValidators: true }
-  )
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
+    app.post("/api/workouts", function (req, res) {
+        Workout.create({})
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
     });
-});
 
-router.get("/api/workouts/range", (req, res) => {
-  Workout.find({})
-    .then(dbWorkout => {
-      // console.log(dbWorkout)
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
     });
-});
-};
+}
--- server.js
+++ server.js
@@ -1,25 +1,31 @@
+// Dependencies
 const express = require("express");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
-const apiRoutes = require("./routes/apiRoute.js");
-const htmlRoutes = require("./routes/htmlRoute.js")
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
+// Setting up Express App
 const app = express();
+const PORT = process.env.PORT || 8000;
+
 app.use(morgan("dev"));
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
 
-app.use(htmlRoutes);
-app.use(apiRoutes);
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// Creating Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
 });
